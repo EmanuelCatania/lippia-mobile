f3c77dab73ece16269a2b69d1a280f679f70f457
lgtmboop <alecarbonari99@gmail.com>
2025-04-29 08:57:50 -0300

    [UPGRADED] LTM Dependency

--- src/main/java/com/crowdar/examples/report/TestManager.java
+++ src/main/java/com/crowdar/examples/report/TestManager.java
@@ -2,13 +2,12 @@ package com.crowdar.examples.report;
 
 import com.crowdar.driver.DriverManager;
 
-
 import io.lippia.reporter.ltm.TestManagerAPIAdapter;
+
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
 public class TestManager extends TestManagerAPIAdapter {
-
 	public TestManager(String arg) {
 		super();
 	}
@@ -17,5 +16,4 @@ public class TestManager extends TestManagerAPIAdapter {
 	public String getBase64Image() {
 		return  ((TakesScreenshot) DriverManager.getDriverInstance()).getScreenshotAs(OutputType.BASE64);
 	}
-
 }
